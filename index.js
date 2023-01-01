12067f211e72434eb2e17c2b238af3c5855c21c0
Lansilvester <alan.lengkong@gmail.com>
2023-01-01 14:55:15 +0800

    Buat POST & GET images

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 const port = 8000;
-
+// Routes
 const destRoutes = require("./src/routes/destinations");
 const authRoutes = require("./src/routes/auth");
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'images');
+    },
+    filename: (req,file,cb) => {
+      cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+  if(
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ){
+    cb(null, true);
+  }else{
+    cb(null, false)
+  }
+}
+
 // Middleware for bodyParser
-app.use(bodyParser.json()) //type JSON
+app.use(bodyParser.json()); //type JSON
+app.use('/images', express.static(path.join(__dirname,'images')))
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+  }).single('image'));
+  
 // Middleware for CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- src/controllers/destinations.js
+++ src/controllers/destinations.js
@@ -3,9 +3,7 @@ const Destination = require('../models/destination');
 
 
 exports.createDest = (req, res, next) => {
-    const title = req.body.title;
-    const description = req.body.description;
-
+    
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
@@ -14,11 +12,21 @@ exports.createDest = (req, res, next) => {
         err.data = errors.array();
         throw err;
     }
-    
+
+    if(!req.file){
+        const err = new Error('Image harus diupload');
+        err.errorStatus = 422;
+        throw err;  
+    }
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.file.path;
+
     // Post data ke mongoDB dengan response API
     const Posting = new Destination({
         title: title,
         description: description,
+        image: image,
         author: {uuid: 1, name: 'Testing'}
     })
     Posting.save()
